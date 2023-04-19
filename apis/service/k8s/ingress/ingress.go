f7675555b966a83b80071b6594e70ef3091e88e4
SoulChild <742899387@qq.com>
2023-04-19 21:49:16 +0800

    fix: ingress list 响应错误数据

--- apis/service/k8s/ingress/ingress.go
+++ apis/service/k8s/ingress/ingress.go
@@ -63,7 +63,7 @@ func (i *Ingress) GetIngressList(filterName, namespace string, limit, page int) 
 		Limit: limit,
 		Page:  page,
 		Total: total,
-		Items: ingresses,
+		Items: selectableData.GenericDataList,
 	}, nil
 }
 
@@ -134,6 +134,80 @@ func (i *Ingress) CreateSimpleIngress(ingressSimpleCreate *dto.K8sIngressSimpleC
 	}
 
 	_, err = global.K8s.ClientSet.NetworkingV1().Ingresses(ing.Namespace).Create(context.TODO(), ing, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
+	return
+}
+
+func (i *Ingress) UpdateSimpleIngress(ingressSimpleCreate *dto.K8sIngressSimpleCreate) (err error) {
+	var rules []ingressv1.IngressRule
+	var tlses []ingressv1.IngressTLS
+
+	// 默认值
+	pathType := ingressv1.PathTypePrefix
+	if ingressSimpleCreate.Annotations != nil {
+		ingressSimpleCreate.Annotations["created-by"] = global.K8sManager
+	} else {
+		ingressSimpleCreate.Annotations = map[string]string{"created-by": global.K8sManager}
+	}
+
+	rule := ingressv1.IngressRule{
+		IngressRuleValue: ingressv1.IngressRuleValue{
+			HTTP: &ingressv1.HTTPIngressRuleValue{Paths: []ingressv1.HTTPIngressPath{
+				{
+					Path:     ingressSimpleCreate.Rule.Path,
+					PathType: &pathType,
+					Backend: ingressv1.IngressBackend{
+						Service: &ingressv1.IngressServiceBackend{
+							Name: ingressSimpleCreate.Rule.Service,
+							Port: ingressv1.ServiceBackendPort{
+								Number: ingressSimpleCreate.Rule.ServicePort,
+							},
+						},
+					},
+				},
+			}},
+		},
+	}
+
+	// 将hosts生成多个IngressRule和TLS
+	if len(ingressSimpleCreate.Rule.Hosts) != 0 {
+		for _, host := range ingressSimpleCreate.Rule.Hosts {
+			rule.Host = host
+			rules = append(rules, rule)
+		}
+	} else {
+		rules = append(rules, rule)
+	}
+
+	// 生成tls配置
+	if ingressSimpleCreate.Tls != nil {
+		for _, item := range ingressSimpleCreate.Tls {
+			tls := ingressv1.IngressTLS{
+				Hosts:      item.Hosts,
+				SecretName: item.SecretName,
+			}
+			tlses = append(tlses, tls)
+		}
+	}
+
+	ing := &ingressv1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        ingressSimpleCreate.Name,
+			Namespace:   ingressSimpleCreate.Namespace,
+			Labels:      map[string]string{},
+			Annotations: ingressSimpleCreate.Annotations,
+		},
+		Spec: ingressv1.IngressSpec{
+			IngressClassName: pointer.String(ingressSimpleCreate.IngressClassName),
+			Rules:            rules,
+			TLS:              tlses,
+		},
+	}
+
+	_, err = global.K8s.ClientSet.NetworkingV1().Ingresses(ing.Namespace).Update(context.TODO(), ing, metav1.UpdateOptions{})
 	if err != nil {
 		return err
 	}
